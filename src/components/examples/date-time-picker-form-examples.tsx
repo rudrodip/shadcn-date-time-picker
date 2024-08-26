3051312ed13843bd0c1397e755d5294286df2c02
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-26 17:09:41 +0600

    init

---
+++ src/components/examples/date-time-picker-form-examples.tsx
@@ -0,0 +1,40 @@
+import { DatePickerForm } from "@/components/date-n-time/form/date-picker-form";
+import { DatePickerWithRangeForm } from "@/components/date-n-time/form/date-picker-range-form";
+import { DateTimePickerFrom } from "@/components/date-n-time/form/date-time-picker-form";
+import { DateTimePicker24hForm } from "@/components/date-n-time/form/date-time-picker-24h-form";
+import { ComponentWrapper } from "./wrapper";
+
+export const FormExamples = () => {
+  return (
+    <div className="w-full space-y-5">
+      <p className="text-lg">
+        Example with{" "}
+        <span className="text-base font-mono border p-px rounded">
+          react-hook-form & zod
+        </span>
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-12 gap-y-8 w-full">
+        <ComponentWrapper
+          Component={DatePickerForm}
+          label="Date Picker Form"
+          slug="date-picker-form"
+        />
+        <ComponentWrapper
+          Component={DatePickerWithRangeForm}
+          label="Date Picker With Range Form"
+          slug="date-picker-range-form"
+        />
+        <ComponentWrapper
+          Component={DateTimePickerFrom}
+          label="Date Time Picker Form"
+          slug="date-time-picker-form"
+        />
+        <ComponentWrapper
+          Component={DateTimePicker24hForm}
+          label="Date Time 24h Picker Form"
+          slug="date-time-24h-picker-form"
+        />
+      </div>
+    </div>
+  );
+}
