9eab5b021f9fc2cb10068ea129898e5dd946e8e3
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-26 22:41:17 +0600

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,26 @@
 import Socials from "@/components/socials";
 import { Examples } from "@/components/examples/date-time-picker-examples";
 import { FormExamples } from "@/components/examples/date-time-picker-form-examples";
+import { ExternalLink } from "@/components/external-link";
+import { siteConfig } from "@/config/site.config";
 
 export default function Home() {
   return (
-    <main className="w-full flex flex-col space-y-12 p-2 md:p-1 lg:p-0">
+    <main className="w-full flex flex-col space-y-12 my-10 p-2 md:p-1 lg:p-0">
       <Header />
       <Examples />
       <FormExamples />
+      <p className="text-sm text-center">
+        made by{" "}
+        <ExternalLink href={siteConfig.creator.url}>rds_agi</ExternalLink>
+      </p>
     </main>
   );
 }
 
 const Header = () => {
   return (
-    <div className="mt-10 w-full flex justify-between">
+    <div className="w-full flex justify-between">
       <h1 className="head-text-md">Date & Time picker</h1>
       <Socials />
     </div>
