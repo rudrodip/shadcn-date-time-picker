bcdd758f0d0e7a24bae0bf053400efb54895e1dd
rudrodip <official.rudrodipsarker@gmail.com>
2024-08-26 17:10:33 +0600

    site config

---
+++ src/config/site.config.ts
@@ -0,0 +1,30 @@
+import type { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "ShadCN Date Time Picker",
+  title: "ShadCN Date Time Picker",
+  description:
+    "ShadCN Date Time Picker is a comprehensive solution for date and time selection, built with Next.js 13/14, Tailwind CSS, and TypeScript.",
+  keywords: [
+    "ShadCN Date Time Picker",
+    "Date Picker",
+    "Time Picker",
+    "Next.js",
+    "Tailwind CSS",
+    "TypeScript",
+    "ShadCN",
+    "Starter",
+    "Template",
+    "Boilerplate",
+  ],
+  creator: {
+    name: "rds_agi",
+    url: "https://rdsx.dev",
+  },
+  siteUrl: "https://time.rdsx.dev",
+  ogImage: "https://time.rdsx.dev",
+  links: {
+    x: "https://x.com/rds_agi",
+    github: "https://github.com/rudrodip/shadcn-date-time-picker",
+  },
+};
